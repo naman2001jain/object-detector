93477eef9edb9fa4da5b219d5bf93c8766c04bdd
naman jain <naman2001jain@gmail.com>
2021-05-27 12:25:56 +0530

    Initial Commit

---
+++ hotdog detector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hotdog detector
+//
+//  Created by Naman Jain on 27/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
